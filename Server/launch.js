208893fbf387a0eff7c1784e2dcf69350b66e743
Joel Ayoub <jnayoub@gmail.com>
2024-08-25 14:42:20 -0400

    update port variable to pull in environment port if deployed to the public internet

--- Server/launch.js
+++ Server/launch.js
@@ -5,7 +5,7 @@ const env = require('dotenv').config();
 const supabase = require('./Connections/supabase');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
